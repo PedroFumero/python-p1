346fd94a86e4fcd2b427d93e4404408bb604e691
Joseandresfr1 <joseandresfr1@gmail.com>
2020-06-13 15:05:37 -0400

    Aislar Responsabilidades

--- generador_resumen.py
+++ generador_resumen.py
@@ -1,13 +1,32 @@
 class Resumen:
     """
-        Clase encargada de manejar la generacion del resumen
+        Clase que modela el resumen
+    """
+    def __init__(self,fecha,total,ventasXpizza,ventasXingrediente):
+       self.fecha = fecha,
+       self.total = total,
+       ventasXpizza = ventasXpizza,
+       ventasXingrediente = ventasXingrediente
+
+    # def generarResumen(self):
+    #     pedidos = self.pedidos   
+    #     resumen = Re
+    #     for pedido in pedidos.values():
+    #         for nombre,fecha,tamanio,ingredientes,precio in pedido.items(): 
+
+class generadorResumen():
+    """
+        Clase que genera el resumen
     """
     def __init__(self,pedidos):
         self.pedidos = pedidos
 
     def mostrarPedidos(self):
-        pedidos = self.pedidos   
-        for pedido in pedidos:
-            print (pedido)
-            for campo in pedidos[pedido]:
-                print (campo,':',pedidos[pedido][campo])                
+        pedidos = self.pedidos  
+        if pedidos is None:
+            print('No hay pedidos por mostrar!') 
+        else:
+            for pedido in pedidos:
+                print (pedido)
+                for campo in pedidos[pedido]:
+                    print (campo,':',pedidos[pedido][campo])            
--- nucleo.py
+++ nucleo.py
@@ -1,6 +1,6 @@
 from cargador_archivo import Manejador
 from procesador_ordenes import ProcesadorOrdenes
-from generador_resumen import Resumen
+from generador_resumen import Resumen,generadorResumen
 
 ruta_archivo = Manejador().getRutaArchivo()
 pedidos = Manejador().cargarArchivo(ruta_archivo)
@@ -8,4 +8,4 @@ pedidos = Manejador().cargarArchivo(ruta_archivo)
 
 
 total_ordenes = ProcesadorOrdenes().procesarPedidos(pedidos)
-Resumen(total_ordenes).mostrarPedidos()
+generadorResumen(total_ordenes).mostrarPedidos()
