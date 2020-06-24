17f8258897e21d6ad560649e3c245a9bd9ddf295
Jose Manuel Romero <josemanuelromerop@gmail.com>
2020-06-24 16:37:04 -0400

    Cargar datos desde BD
    Se añadió un metodo para obtener datos de la BD, se añade una opcion al
    menu para ver los datos de la BD y se crearon funciones para validar la
    existencia de datos en BD y evitar datos duplicados

--- nucleo.py
+++ nucleo.py
@@ -5,8 +5,13 @@ from procesador_ordenes import ProcesadorOrdenes
 from generador_resumen import ResumenDelDia,GeneradorResumen
 from database_controller import DatabaseController
 
+# Inicializacion de la Base de Datos
+db = DatabaseController('pizzeria_database.db')
+existe_db = db.tiene_datos()
+print(existe_db)
+
 # Menú de opciones
-opt = Manejador().menu(0)
+opt = Manejador().menu(0, existe_db)
 if opt == '1':
     # Cargar pedidos desde un archivo
     ruta_archivo = Manejador().getRutaArchivo()
@@ -15,19 +20,29 @@ elif opt == '2':
     # Cargar todos los pedidos en la carpeta /misc
     pedidos = ProcesadorOrdenes().procesarTodos()
     # print(pedidos)
+elif opt == '3' and existe_db:
+    # Cargar desde la base de datos
+    pedidos = db.obtenerPedidos()
+
 
 # TODO Cambiar nombre de método a validar() solamente, porque se estan validando varias cosas
 if Manejador().validarVacio(pedidos):
     sys.exit()
     
-
-db = DatabaseController('pizzeria_database.db')
-db.cargar_registros(pedidos)
-# db.print_datase()
-
 total_ordenes = ProcesadorOrdenes().procesarPedidos(pedidos)
 #generadorResumen(total_ordenes).mostrarPedidos()
 resumenXfecha = GeneradorResumen(total_ordenes).generarListaResumen()
 
 for dia in resumenXfecha:
     dia.mostrarResumen()
+
+# Cargar datos a BD si no vienen de la BD
+if opt != '3':
+    print("¿Desa cargar estos datos en la base de datos?")
+    print("Considere que cargar varias veces el mismo archivo puede generar datos duplicados")
+    opt_db = input('[si/no]: ')
+    if opt_db.lower() in ['y', 'yes', 's', 'si', 'sí']:
+        db.cargar_registros(pedidos)
+
+#print(pedidos)
+#db.print_datase()
